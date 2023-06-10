37f63aaf808924230a661f0d69d334fb0cae7280
Enrico Robazza <enricorobazzi@gmail.com>
2023-06-10 12:34:12 -0300

    Chromakey

---
+++ server.py
@@ -0,0 +1,11 @@
+from http.server import SimpleHTTPRequestHandler
+import ssl
+from socketserver import TCPServer
+
+httpd = TCPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+
+httpd.socket = ssl.wrap_socket (httpd.socket, 
+        keyfile="./.cert/key.pem", 
+        certfile='./.cert/cert.pem', server_side=True)
+
+httpd.serve_forever()
