82f75e46c5c15baf23724c59427e0947b377f027
Enrico Robazza <enricorobazzi@gmail.com>
2023-06-11 01:23:58 -0300

    Grassman

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from http.server import SimpleHTTPRequestHandler
 import ssl
 from socketserver import TCPServer
 
-httpd = TCPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+httpd = TCPServer(('0.0.0.0', 8001), SimpleHTTPRequestHandler)
 
 httpd.socket = ssl.wrap_socket (httpd.socket, 
         keyfile="./.cert/key.pem", 
